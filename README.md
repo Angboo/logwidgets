7eaaa6f304da9d40b76a66a448b82881245e4dec
Ang Zhang <angboozhang@gmail.com>
2022-06-26 15:10:09 +0800

    [Angboo] Improve GTD Matrix: add gtd task query tempalte and refact css

--- README.md
+++ README.md
@@ -23,11 +23,18 @@
 ## Features
 
 - [x] GTD Matrix
+    - [x] Insert GTD Matrix
+    - [x] Insert GTD Task Query Statement
 
 ## GTD Matrix
 
 The function-related styles are derived from cannibalox's
-article [\[css+template\] eisenhower matrix](https://discuss.logseq.com/t/css-template-eisenhower-matrix/526) <br/>
+article [\[css+template\] eisenhower matrix](https://discuss.logseq.com/t/css-template-eisenhower-matrix/526) 
+<br/><br/>
 If you need more friendly view result through the query function, please refer to
 article [OKR + GTD + Note => Logseq](https://www.bmpi.dev/self/okr-gtd-note-logseq/)
+
 ![GTD Matrix](./demo/gtd-matrix.gif)
+
+### Known issues
+- [ ] Slash command list does not appear properly in the matrix
--- src/matrix.ts
+++ src/matrix.ts
@@ -5,6 +5,7 @@ import {IBatchBlock} from "@logseq/libs/dist/LSPlugin";
 const importGtdMatrix = () => {
     logseq.provideStyle(matrixStyle)
     logseq.Editor.registerSlashCommand("GTD Matrix", insertGtdMatrixTemplate)
+    logseq.Editor.registerSlashCommand("GTD Task Query", insertTaskQuery)
 };
 
 const insertGtdMatrixTemplate = async () => {
@@ -14,8 +15,15 @@ const insertGtdMatrixTemplate = async () => {
     }
 }
 
+const insertTaskQuery = async () => {
+    const targetBlock = await logseq.Editor.getCurrentBlock()
+    if (targetBlock != null) {
+        await logseq.Editor.insertBlock(targetBlock.uuid, gtdTaskQueryTemplate, {before: true})
+    }
+}
+
 const gtdMatrixTemplate: IBatchBlock = {
-    content: "## #.v-gtd-matrix\ntemplate:: v-eisenhower",
+    content: "## #.v-gtd-matrix",
     children: [
         {"content": "### [[TODO]]", children: [{content: "#### example"}]},
         {"content": "### [[DECIDE]]", children: [{content: "#### example"}]},
@@ -23,6 +31,18 @@ const gtdMatrixTemplate: IBatchBlock = {
         {"content": "### [[ELIMINATE]]", children: [{content: "#### example"}]}
     ]
 };
+
+const gtdTaskQueryTemplate: string = `#+BEGIN_QUERY
+ {:title [:b "Query"]
+ :query [:find (pull ?b [*])
+ :where
+ [task ?b #{"LATER","TODO","DOING","NOW","DONE"}]
+ [priority ?b #{"A","B","C"}]
+]
+ :table-view? false
+ }
+ #+END_QUERY`
+
 
 export default importGtdMatrix
 
